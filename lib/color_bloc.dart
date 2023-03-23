0f396b55a5dde3fb0d9ccef15b00d6bd30d3fe98
Taras Bobriienko <taras3189@gmail.com>
2023-03-23 12:58:37 +0200

    Second commit

---
+++ lib/color_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class ColorEvent {}
+
+class RedColorEvent extends ColorEvent {}
+
+class GreenColorEvent extends ColorEvent {}
+
+class ColorBloc extends Bloc<ColorEvent, Color> {
+  ColorBloc() : super(Colors.red) {
+    on<RedColorEvent>((event, emit) => emit(Colors.red));
+    on<GreenColorEvent>((event, emit) => emit(Colors.green));
+  }
+}
